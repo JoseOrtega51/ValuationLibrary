2a7b361a57e9a20f1d7c82371fbe1610ef0777db
José Ortega Moya <114946471+JoseOrtega51@users.noreply.github.com>
2024-11-28 23:36:31 +0100

    new class IRCurve and modify setup.py

--- src/MarketModel.cpp
+++ src/MarketModel.cpp
@@ -1,5 +1,18 @@
 #include "MarketModel.h"
 
-double MarketModel::getRiskFreeRate (double t){
-    return _risk_free_rate;
+
+namespace ValLry{
+
+    IRCurve::IRCurve(std::vector<double> buckets, std::vector<double> rates) : _buckets(buckets), _rates(rates){
+        _spline3_coefs = spline3_natural_coefs(_buckets, _rates);
+    }
+
+    double IRCurve::getValue(double t){
+        return spline3_natural_eval(_buckets, _rates, _spline3_coefs, t);
+    }
+
+    double MarketModel::getRiskFreeRate (double t){
+        return _risk_free_rate;
+    }
+
 }
--- src/MarketModel.h
+++ src/MarketModel.h
@@ -1,15 +1,29 @@
 #ifndef MARKET_MODEL_H
 #define MARKET_MODEL_H
 
-#include <map>
+#include <vector>
+#include <deque>
+#include "ValuationMathTools.h"
 
-class MarketModel {
-    private:
-        double _risk_free_rate;
-        std::map <double,double> _risk_free_curve; //rate[tenor]
+namespace ValLry{
+    class IRCurve {
+        public:
+            std::vector<double> _buckets;
+            std::vector<double> _rates;
+            std::deque<double> _spline3_coefs;
 
-    public:
-        double getRiskFreeRate( double t );
-};
+            IRCurve(std::vector<double> buckets, std::vector<double> rates);
 
+            double getValue(double t);
+    };
+
+    class MarketModel {
+        private:
+            double _risk_free_rate;
+            IRCurve _risk_free_curve; //rate[tenor]
+
+        public:
+            double getRiskFreeRate( double t );
+    };
+}
 #endif
