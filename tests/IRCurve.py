92b5f1dd5c0869da49e5e0770e062f8d1555684c
José Ortega Moya <114946471+JoseOrtega51@users.noreply.github.com>
2025-04-20 00:05:39 +0200

    modify IRCurve.py to test new functionality

--- tests/IRCurve.py
+++ tests/IRCurve.py
@@ -13,3 +13,17 @@ rate_v = curve.getValue(t)
 plt.plot(t, rate_v)
 plt.plot(buckets,rates,'o')
 plt.show()
+
+#######################################
+
+buckets_str = ["6M", "1Y", "2Y", "3Y", "5Y", "10Y"]
+rates = np.array([0.2, 0.25, 0.28, 0.29, 0.3, 0.32])
+curve = ValLry.IRCurve(buckets_str, rates)
+
+t = np.linspace(0.5,10.,100)
+r_t = np.zeros(100)
+
+rate_v = curve.getValue(t)
+plt.plot(t, rate_v)
+plt.plot(buckets,rates,'o')
+plt.show()
